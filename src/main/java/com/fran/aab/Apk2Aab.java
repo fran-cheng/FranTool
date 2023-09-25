fac9fa62d542b0bb581b58af4b835b9375f2ee0e
Fran <1093451587@qq.com>
2023-09-25 20:26:14 +0800

    逻辑初步完成

--- src/main/java/com/fran/aab/Apk2Aab.java
+++ src/main/java/com/fran/aab/Apk2Aab.java
@@ -3,12 +3,19 @@ package com.fran.aab;
 import com.fran.util.RuntimeHelper;
 import com.fran.util.Utils;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
+import brut.androlib.Config;
 import brut.common.BrutException;
 import brut.util.AaptManager;
 import brut.util.Jar;
@@ -21,27 +28,31 @@ import brut.util.Jar;
  **/
 
 public class Apk2Aab {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Apk2Aab aab = new Apk2Aab();
-		aab.compile();
+//		aab.compile();
 //		aab.linkSources();
+//		aab.unZipBase();
+		aab.copySources();
 		// TODO: 2023/9/8 解压 base.apk
+	}
+
+
+	private String getAapt2Path() {
+		try {
+			File aapt = AaptManager.getAapt2();
+			return aapt.getPath();
+		} catch (BrutException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	/**
 	 * 编译资源
 	 */
 	private void compile() {
-		String aaptPath;
-
-		try {
-			File aapt = AaptManager.getAapt2();
-			aaptPath = aapt.getPath();
-		} catch (BrutException e) {
-			throw new RuntimeException(e);
-		}
-
+		String aaptPath = getAapt2Path();
 		String apkDecodePath = "D:\\FranGitHub\\FranTool\\runtime\\20230922-X2-gf";
 		String cmdCompile = String.format("%s compile --legacy --dir %s -o compiled_resources.zip", aaptPath, Utils.linkPath(apkDecodePath, "res"));
 		try {
@@ -55,9 +66,12 @@ public class Apk2Aab {
 	 * 关联资源
 	 */
 	private void linkSources() {
-		String apkDecodePath = "D:\\FranGitHub\\FranTool\\Runtime\\app-debug";
-		String aaptPath = "D:\\FranGitHub\\FranTool\\tool\\aab-tool\\aapt2.exe";
+
+		String apkDecodePath = "D:\\FranGitHub\\FranTool\\runtime\\20230922-X2-gf";
+		String aaptPath = getAapt2Path();
 		String androidJarPath = "D:\\FranGitHub\\FranTool\\tool\\aab-tool\\android.jar";
+//		String androidJarPath = Config.getDefaultConfig().frameworkDirectory;
+
 		String minVersion = "24";
 		String targetVersion = "31";
 		String versionCode = "1";
@@ -74,24 +88,78 @@ public class Apk2Aab {
 
 	/**
 	 * 解压base.apk
+	 * 通过unzip解压到base文件夹，目录结构：
 	 */
-	private void unZipBase() {
-		String aaptPath = "D:\\FranGitHub\\FranTool\\tool\\aab-tool\\aapt2.exe";
-		String unZipPath = "D:\\FranGitHub\\FranTool\\tool\\aab-tool\\aapt2.exe";
-		String apkDecodePath = "D:\\FranGitHub\\FranTool\\Runtime\\app-debug";
-		String cmdUnZip = String.format("%s compile --dir %s -o compiled_resources.zip", aaptPath, Utils.linkPath(apkDecodePath, "res"));
+	private String unZipBase() {
+
+		String zipFilePath = "D:\\FranGitHub\\FranTool\\base.apk";
+		String destDirectory = "D:\\FranGitHub\\FranTool\\runtime\\20230922-X2-gf\\base";
+		byte[] buffer = new byte[1024];
 		try {
-			RuntimeHelper.getInstance().run(cmdUnZip);
-		} catch (Exception e) {
+			// 创建解压缩输入流
+			FileInputStream fis = new FileInputStream(zipFilePath);
+			ZipInputStream zis = new ZipInputStream(fis);
+			ZipEntry zipEntry = zis.getNextEntry();
+			while (zipEntry != null) {
+				String fileName = zipEntry.getName();
+				File newFile = new File(destDirectory + File.separator + fileName);
+				// 创建目录
+				new File(newFile.getParent()).mkdirs();
+				FileOutputStream fos = new FileOutputStream(newFile);
+				int len;
+				while ((len = zis.read(buffer)) > 0) {
+					fos.write(buffer, 0, len);
+				}
+				fos.close();
+				zipEntry = zis.getNextEntry();
+			}
+			zis.closeEntry();
+			zis.close();
+			fis.close();
+			System.out.println("解压缩完成");
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+
+		return destDirectory;
 	}
 
 	/**
 	 * 拷贝资源
 	 */
-	private void copySources() {
+	private void copySources() throws IOException {
 //		创建 base/manifest 将 base/AndroidManifest.xml 剪切过来
 		File manifesFile = new File("");
+
+		String apkDecodeBasePath = "D:\\FranGitHub\\FranTool\\runtime\\20230922-X2-gf\\base";
+		String apkDecodePath = "D:\\FranGitHub\\FranTool\\runtime\\20230922-X2-gf";
+
+		File androidManifestFile = new File(Utils.linkPath(apkDecodeBasePath, "AndroidManifest.xml"));
+		if (androidManifestFile.exists()) {
+			Utils.copyOperation(androidManifestFile, new File(Utils.linkPath(apkDecodeBasePath, "manifest", "AndroidManifest.xml")));
+		}
+
+		File assetsFile = new File(Utils.linkPath(apkDecodePath, "assets"));
+		if (assetsFile.exists()) {
+			Utils.copyOperation(assetsFile, new File(Utils.linkPath(apkDecodeBasePath, "assets")));
+		}
+
+		File libFile = new File(Utils.linkPath(apkDecodePath, "lib"));
+		if (libFile.exists()) {
+			Utils.copyOperation(libFile, new File(Utils.linkPath(apkDecodeBasePath, "lib")));
+		}
+
+		File unknownFile = new File(Utils.linkPath(apkDecodePath, "unknown"));
+		if (unknownFile.exists()) {
+			Utils.copyOperation(unknownFile, new File(Utils.linkPath(apkDecodeBasePath, "root")));
+		}
+
+
+		File kotlinFile = new File(Utils.linkPath(apkDecodePath, "kotlin"));
+		if (kotlinFile.exists()) {
+			Utils.copyOperation(kotlinFile, new File(Utils.linkPath(apkDecodeBasePath, "root")));
+		}
+
 	}
 }
--- src/main/java/com/fran/util/Utils.java
+++ src/main/java/com/fran/util/Utils.java
@@ -12,216 +12,216 @@ import java.util.Scanner;
  * * 说明:工具
  **/
 public class Utils {
-    private static boolean info = false;
+	private static boolean info = false;
 
-    public static void main(String[] args) {
-        String apk = "APK´ò°ü";
-        String apk2 = "APK合并";
-        try {
-           System.out.println( new String(apk.getBytes(StandardCharsets.ISO_8859_1),"GBK"));
-           System.out.println( new String(apk2.getBytes("GBK"),StandardCharsets.ISO_8859_1));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		String apk = "APK´ò°ü";
+		String apk2 = "APK合并";
+		try {
+			System.out.println(new String(apk.getBytes(StandardCharsets.ISO_8859_1), "GBK"));
+			System.out.println(new String(apk2.getBytes("GBK"), StandardCharsets.ISO_8859_1));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * 日志
-     *
-     * @param s String
-     */
-    public static void log(String s) {
-        System.out.println(s);
-    }
+	/**
+	 * 日志
+	 *
+	 * @param s String
+	 */
+	public static void log(String s) {
+		System.out.println(s);
+	}
 
-    public static void logInfo(String s) {
-        if (info) {
-            System.out.println(s);
-        }
-    }
+	public static void logInfo(String s) {
+		if (info) {
+			System.out.println(s);
+		}
+	}
 
-    /**
-     * 日志
-     *
-     * @param tag String
-     * @param s   String
-     */
-    static void log(String tag, String s) {
-        System.out.println(tag + " :  " + s);
-    }
+	/**
+	 * 日志
+	 *
+	 * @param tag String
+	 * @param s   String
+	 */
+	static void log(String tag, String s) {
+		System.out.println(tag + " :  " + s);
+	}
 
-    /**
-     * SHA-1
-     *
-     * @param strSrc String
-     * @param len    String
-     * @return String
-     */
-    public static String sha1(String strSrc, int len) {
-        MessageDigest md;
-        String strDes;
-        byte[] bt = strSrc.getBytes();
+	/**
+	 * SHA-1
+	 *
+	 * @param strSrc String
+	 * @param len    String
+	 * @return String
+	 */
+	public static String sha1(String strSrc, int len) {
+		MessageDigest md;
+		String strDes;
+		byte[] bt = strSrc.getBytes();
 
-        try {
-            md = MessageDigest.getInstance("SHA-1");
-            md.update(bt);
-            strDes = bytes2Hex(md.digest());
-        } catch (NoSuchAlgorithmException var6) {
-            return null;
-        }
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+			md.update(bt);
+			strDes = bytes2Hex(md.digest());
+		} catch (NoSuchAlgorithmException var6) {
+			return null;
+		}
 
-        return len == 0 ? strDes : strDes.substring(0, len);
-    }
+		return len == 0 ? strDes : strDes.substring(0, len);
+	}
 
-    /**
-     * bytes2Hex
-     *
-     * @param bts byte
-     * @return String
-     */
-    private static String bytes2Hex(byte[] bts) {
-        StringBuilder stringBuilder = new StringBuilder();
-        String tmp;
+	/**
+	 * bytes2Hex
+	 *
+	 * @param bts byte
+	 * @return String
+	 */
+	private static String bytes2Hex(byte[] bts) {
+		StringBuilder stringBuilder = new StringBuilder();
+		String tmp;
 
-        for (byte bt : bts) {
-            tmp = Integer.toHexString(bt & 255);
-            if (tmp.length() == 1) {
-                stringBuilder.append("0");
-            }
+		for (byte bt : bts) {
+			tmp = Integer.toHexString(bt & 255);
+			if (tmp.length() == 1) {
+				stringBuilder.append("0");
+			}
 
-            stringBuilder.append(tmp);
-        }
+			stringBuilder.append(tmp);
+		}
 
-        return stringBuilder.toString();
-    }
+		return stringBuilder.toString();
+	}
 
-    /**
-     * 生成随机数
-     *
-     * @param min min
-     * @param max max
-     * @return int
-     */
-    public static int random(int min, int max) {
-        return min + (int) Math.round(Math.random() * (double) (max - min));
-    }
+	/**
+	 * 生成随机数
+	 *
+	 * @param min min
+	 * @param max max
+	 * @return int
+	 */
+	public static int random(int min, int max) {
+		return min + (int) Math.round(Math.random() * (double) (max - min));
+	}
 
-    /**
-     * 写文件
-     *
-     * @param file    File
-     * @param content String
-     * @param charset String
-     */
-    public static void writeFile(File file, String content, String charset) {
-        OutputStream os;
+	/**
+	 * 写文件
+	 *
+	 * @param file    File
+	 * @param content String
+	 * @param charset String
+	 */
+	public static void writeFile(File file, String content, String charset) {
+		OutputStream os;
 
-        try {
-            if (!file.exists()) {
-                makeIfDir(file);
-                file.createNewFile();
-            }
+		try {
+			if (!file.exists()) {
+				makeIfDir(file);
+				file.createNewFile();
+			}
 
-            os = new FileOutputStream(file);
-            os.write(content.getBytes(charset));
-            os.close();
-        } catch (Exception var6) {
-            var6.printStackTrace();
-        }
-    }
+			os = new FileOutputStream(file);
+			os.write(content.getBytes(charset));
+			os.close();
+		} catch (Exception var6) {
+			var6.printStackTrace();
+		}
+	}
 
-    /**
-     * 生成dir
-     *
-     * @param f File
-     */
-    private static void makeIfDir(File f) {
-        if (f.isDirectory()) {
-            if (!f.exists()) {
-                f.mkdirs();
-            }
-        } else {
-            File dir = f.getParentFile();
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
-        }
+	/**
+	 * 生成dir
+	 *
+	 * @param f File
+	 */
+	private static void makeIfDir(File f) {
+		if (f.isDirectory()) {
+			if (!f.exists()) {
+				f.mkdirs();
+			}
+		} else {
+			File dir = f.getParentFile();
+			if (!dir.exists()) {
+				dir.mkdirs();
+			}
+		}
 
-    }
+	}
 
-    /**
-     * 读文件
-     *
-     * @param f File
-     * @return String
-     */
-    public static String read(File f) {
-        String c = null;
+	/**
+	 * 读文件
+	 *
+	 * @param f File
+	 * @return String
+	 */
+	public static String read(File f) {
+		String c = null;
 
-        try {
-            InputStream fis = new FileInputStream(f);
-            c = inputStreamToString(fis);
-            fis.close();
-        } catch (Exception var5) {
-            var5.printStackTrace();
-        }
+		try {
+			InputStream fis = new FileInputStream(f);
+			c = inputStreamToString(fis);
+			fis.close();
+		} catch (Exception var5) {
+			var5.printStackTrace();
+		}
 
-        return c;
-    }
+		return c;
+	}
 
-    /**
-     * inputStreamToString
-     *
-     * @param is InputStream
-     * @return String
-     */
-    private static String inputStreamToString(InputStream is) {
+	/**
+	 * inputStreamToString
+	 *
+	 * @param is InputStream
+	 * @return String
+	 */
+	private static String inputStreamToString(InputStream is) {
 
-        Scanner scanner = new Scanner(is);
-        StringBuilder stringBuilder = new StringBuilder();
-        while (scanner.hasNext())
-            stringBuilder.append(scanner.next());
-
-
-        return stringBuilder.toString();
-    }
-
-    /**
-     * 拼接地址
-     *
-     * @param basePath 基础路径
-     * @return path
-     */
+		Scanner scanner = new Scanner(is);
+		StringBuilder stringBuilder = new StringBuilder();
+		while (scanner.hasNext())
+			stringBuilder.append(scanner.next());
 
 
-    public static String linkPath(String basePath, String... dentrys) {
-        StringBuilder stringBuilder = new StringBuilder(basePath);
-        for (String dentry : dentrys) {
-            stringBuilder.append(File.separator);
-            stringBuilder.append(dentry);
-        }
-        return stringBuilder.toString();
-    }
+		return stringBuilder.toString();
+	}
 
-    /**
-     * 拷贝的具体操作
-     *
-     * @param tempFile   输入
-     * @param outPutFile 输出
-     */
-    private void copyOperation(File tempFile, File outPutFile) {
-        try (FileReader fileReader = new FileReader(tempFile);
-             FileWriter fileWriter = new FileWriter(outPutFile)) {
-            char[] chars = new char[1024];
-            int length;
-            while ((length = fileReader.read(chars)) != -1) {
-                fileWriter.write(chars, 0, length);
-            }
-            fileWriter.flush();
+	/**
+	 * 拼接地址
+	 *
+	 * @param basePath 基础路径
+	 * @return path
+	 */
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+
+	public static String linkPath(String basePath, String... dentrys) {
+		StringBuilder stringBuilder = new StringBuilder(basePath);
+		for (String dentry : dentrys) {
+			stringBuilder.append(File.separator);
+			stringBuilder.append(dentry);
+		}
+		return stringBuilder.toString();
+	}
+
+	/**
+	 * 拷贝的具体操作
+	 *
+	 * @param tempFile   输入
+	 * @param outPutFile 输出
+	 */
+	public static void copyOperation(File tempFile, File outPutFile) {
+		try (FileReader fileReader = new FileReader(tempFile);
+			 FileWriter fileWriter = new FileWriter(outPutFile)) {
+			char[] chars = new char[1024];
+			int length;
+			while ((length = fileReader.read(chars)) != -1) {
+				fileWriter.write(chars, 0, length);
+			}
+			fileWriter.flush();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
