7a438fbead68e3c9fe282481c9431d456a2dc040
Fran <1093451587@qq.com>
2023-10-25 15:50:30 +0800

    临时写的脚本用来替换strings.xml

--- src/main/java/com/fran/MainTest.java
+++ src/main/java/com/fran/MainTest.java
@@ -2,12 +2,18 @@ package com.fran;
 
 
 import com.fran.util.Utils;
-import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
 
-import java.io.File;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author 程良明
@@ -15,91 +21,111 @@ import java.util.Map;
  * * * 说明:测试用
  **/
 public class MainTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws DocumentException {
+		// TODO: 2023/10/24 解析string.xml  ，替换内容，从数组
+		String stringPath = "C:\\Users\\Administrator\\Desktop\\work\\xh\\string\\values-zh\\strings.xml";
+		SAXReader saxReader = new SAXReader();
+		Document workDocument = saxReader.read(stringPath);
 
+		List<Element> elements = workDocument.getRootElement().elements();
 
-		String str = Utils.read(new File("/Users/fran/JavaProject/FranTool/YYGGames.js"));
+		String[] values = {"海外测试",
+						"账号销毁",
+						"邮箱",
+						"密码",
+						"验证码",
+						"发送",
+						"提交",
+						"绑定邮箱",
+						"用户协议",
+						"隐私协议",
+						"确定退出？",
+						"取消",
+						"确认",
+						"提示",
+						"同意",
+						"继续",
+						"绑定",
+						"忘记密码?",
+						"游客模式",
+						"登录",
+						"登陆中",
+						"切换账号",
+						"旧密码",
+						"新密码",
+						"修改密码",
+						"找回密码",
+						"请输入账号邮箱用于验证，检查邮件文件夹包括垃圾箱",
+						"确认发送",
+						"确认",
+						"前往绑定",
+						"Facebook绑定",
+						"Google绑定",
+						"解绑后google不能再登录此账号，确定解绑吗？",
+						"解绑后facebook不能再登录此账号，确定解绑吗？",
+						"绑定成功",
+						"解绑成功",
+						"已绑定",
+						"是否确定注销账号，账号数据将彻底删除",
+						"不再提示",
+						"注册",
+						"注册",
+						"邮箱",
+						"请同意",
+						"和",
+						"网络错误，请重试",
+						"登录成功",
+						"邮箱绑定成功",
+						"请输入邮箱和密码",
+						"注册成功",
+						"请输入邮箱",
+						"请输入密码",
+						"请输入验证码",
+						"请同意用户协议",
+						"游客登录",
+						"Facebook登录",
+						"Google登录",
+						"请输入旧密码",
+						"请输入新密码",
+						"请输入验证码",
+						"找回成功，请重新登录",
+						"支付成功",
+						"支付取消",
+						"支付失败",
+						"修改密码成功，重新登录",
+						"请输入:我确认删除账号",
+						"我确认删除账号",
+						"内容不正确，请重新输入"};
 
-		Map<String, String> map = getHexStrFormString(str);
-
-		for (String key : map.keySet()) {
-			str = str.replace(key, map.get(key));
+		int i = 0;
+		for (Element element : elements) {
+			String name = element.attributeValue("name");
+			String value = element.getStringValue();
+			String targetValue = values[i];
+			element.setText(targetValue);
+			i++;
 		}
-
-		Utils.writeFile(new File("/Users/fran/JavaProject/FranTool/Clm_YYGGames.js"), str, "utf-8");
+		writeXmlFile(stringPath, workDocument);
 
 	}
 
-	/**
-	 * 提取16进制 '\x 开头的字符串，并转换成string
-	 *
-	 * @param str 输入
-	 * @return Map<String, String>   16进制字符串与string的映射
-	 */
-	private static Map<String, String> getHexStrFormString(String str) {
-		Map<String, String> hexStrMapStr = new HashMap<>();
-		String key = "\\x";
-		int tempStartIndex = 0;
-		while (true) {
-			int startIndex = str.indexOf("'" + key, tempStartIndex);
-			tempStartIndex = startIndex + 1;
-			int endIndex = str.indexOf("'", tempStartIndex);
-			if (startIndex < 0 || endIndex < 0) {
-				break;
+	private static void writeXmlFile(String outPutPath, Document document) {
+		Utils.log("写入: " + outPutPath);
+		XMLWriter writer = null;
+		try (BufferedOutputStream fileWriter = new BufferedOutputStream(new FileOutputStream(outPutPath))) {
+			writer = new XMLWriter(fileWriter, OutputFormat.createPrettyPrint());
+			writer.write(document);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (writer != null) {
+					writer.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			String targetStr = str.substring(tempStartIndex, endIndex);
-			tempStartIndex = endIndex;
-			hexStrMapStr.put(targetStr, hexStr2String(targetStr));
 		}
-
-		return hexStrMapStr;
-	}
-
-	private static void test() {
-		// TODO: 2023/5/6 字符串转16进制
-
-		String str = "程良明";
-
-		String builder = str2HexString(str);
-
-		System.out.println("clm 2bin : " + builder);
-
-		String origin = hexStr2String(builder);
-		System.out.println("clm origin: " + origin);
-
-		String hexStr = HexBin.encode(str.getBytes(StandardCharsets.UTF_8));
-		System.out.println("clm hexStr: " + hexStr);
-		System.out.println("clm hexStr2: " + new String(HexBin.decode(hexStr), StandardCharsets.UTF_8));
-	}
-
-	/**
-	 * 字符串转16进制
-	 *
-	 * @param str
-	 * @return
-	 */
-	private static String str2HexString(String str) {
-		StringBuilder builder = new StringBuilder();
-		for (char c : str.toCharArray()) {
-			builder.append("\\x").append(Integer.toHexString(c));
-		}
-		return builder.toString();
-	}
-
-	private static String hexStr2String(String hexStr) {
-		StringBuilder builder = new StringBuilder();
-		String[] strings = hexStr.split("\\\\x");
-
-		for (String str : strings) {
-			if (str.length() > 0) {
-				builder.append(str);
-			}
-		}
-		System.out.println("clm hexStr2String: " + hexStr);
-		System.out.println("clm builder: " + builder.toString());
-		String target = new String(HexBin.decode(builder.toString().replace("\\", "")), StandardCharsets.UTF_8);
-		System.out.println("clm hexStr2StringTarget: " + target);
-		return target;
 	}
 
 }
