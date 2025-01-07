088fae3d487c82709871632194f6e80f8644cd59
Fran <1093451587@qq.com>
2025-01-07 11:12:38 +0800

    提交aes

--- src/main/java/com/fran/tool/AESTool.java
+++ src/main/java/com/fran/tool/AESTool.java
@@ -1,7 +1,6 @@
 package com.fran.tool;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
@@ -14,32 +13,32 @@ import javax.crypto.spec.SecretKeySpec;
  * * * 说明: AES加解密工具类
  **/
 public class AESTool {
-	private String algorithm = "AES/CBC/PKCS5Padding";
-	private byte[] key = "DodXhJoyGamesSdk".getBytes(StandardCharsets.UTF_8);
-	private byte[] iv = "Fran_ChengXhGame".getBytes(StandardCharsets.UTF_8);
+	private final String ALGORITHM = "AES/CBC/PKCS5Padding";
+	private final byte[] KEY = "DodXhJoyGamesSdk".getBytes(StandardCharsets.UTF_8);
+	private final byte[] IV = "Fran_ChengXhGame".getBytes(StandardCharsets.UTF_8);
 
 	public static void main(String[] args) throws Exception {
 		String str = "clm";
 		AESTool aesTool = new AESTool();
 		String encryptStr = aesTool.encrypt(str.getBytes(StandardCharsets.UTF_8));
-		String decryptStr = Arrays.toString(aesTool.decrypt(encryptStr));
-		System.out.println("encryptStr："+encryptStr);
-		System.out.println("decryptStr："+decryptStr);
+		String decryptStr = new String(aesTool.decrypt(encryptStr));
+		System.out.println("encryptStr：" + encryptStr);
+		System.out.println("decryptStr：" + decryptStr);
 	}
 
 	public String encrypt(byte[] plaintext) throws Exception {
-		Cipher cipher = Cipher.getInstance(algorithm);
-		SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
-		IvParameterSpec ivSpec = new IvParameterSpec(iv);
+		Cipher cipher = Cipher.getInstance(ALGORITHM);
+		SecretKeySpec keySpec = new SecretKeySpec(KEY, "AES");
+		IvParameterSpec ivSpec = new IvParameterSpec(IV);
 		cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
 		byte[] encryptedBytes = cipher.doFinal(plaintext);
 		return Base64.getEncoder().encodeToString(encryptedBytes);
 	}
 
 	public byte[] decrypt(String ciphertext) throws Exception {
-		Cipher cipher = Cipher.getInstance(algorithm);
-		SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
-		IvParameterSpec ivSpec = new IvParameterSpec(iv);
+		Cipher cipher = Cipher.getInstance(ALGORITHM);
+		SecretKeySpec keySpec = new SecretKeySpec(KEY, "AES");
+		IvParameterSpec ivSpec = new IvParameterSpec(IV);
 		cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
 		byte[] decodedBytes = Base64.getDecoder().decode(ciphertext);
 		return cipher.doFinal(decodedBytes);
